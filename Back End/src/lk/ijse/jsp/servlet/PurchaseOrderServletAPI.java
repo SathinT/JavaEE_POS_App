4580be1e48c5bb875610aeccc6c944f741bdb959
Sathin Thenuja <sathinthenuja@gmail.com>
2023-08-28 10:02:41 +0530

    commit

--- Back End/src/lk/ijse/jsp/servlet/PurchaseOrderServletAPI.java
+++ Back End/src/lk/ijse/jsp/servlet/PurchaseOrderServletAPI.java
@@ -1,9 +1,40 @@
 package lk.ijse.jsp.servlet;
 
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @WebServlet(urlPatterns = {"/pages/purchase-order"})
 public class PurchaseOrderServletAPI extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)throws IOException{
+        resp.addHeader("Content-Type", "application/json");
+        resp.addHeader("Access-Control-Allow-Origin", "*");
+
+        JsonReader reader = Json.createReader(req.getReader());
+        JsonObject jsonObject = reader.readObject();
+
+        String orderID = jsonObject.getString("orderID");
+        String date = jsonObject.getString("date");
+        JsonObject customer = jsonObject.getJsonObject("customer");
+        JsonArray cart = jsonObject.getJsonArray("cart");
+        String total = jsonObject.getString("total");
+        String discount = jsonObject.getString("discount");
+
+        if (discount.equals("NaN")) {
+            discount = "0";
+        }
+
+        String customerID = customer.getString("id");
+
+        System.out.println(cart);
+    }
+
 
 }
